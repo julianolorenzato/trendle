48f014fcb9c3e4c14609dabd3bfe664cb8e67ca3
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-05-09 19:48:57 -0300

    feat: Poll - NewPoll(), AddOption(), Results(), Vote()

---
+++ poll/poll.go
@@ -0,0 +1,73 @@
+package poll
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type Vote struct {
+	ID      string
+	VoterID string
+}
+
+type Poll struct {
+	ID       string
+	Question string
+	Options  map[string][]Vote
+}
+
+func NewPoll(question string, options []string) (*Poll, error) {
+	if len(question) < 2 {
+		return nil, errors.New("poll question must have at least 2 characters")
+	}
+
+	if len(options) < 1 {
+		return nil, errors.New("poll must have at least 1 option")
+	}
+
+	p := &Poll{
+		ID:       uuid.NewString(),
+		Question: question,
+		Options:  make(map[string][]Vote),
+	}
+
+	for _, v := range options {
+		p.Options[v] = make([]Vote, 2)
+	}
+
+	return p, nil
+}
+
+func (p *Poll) AddOption(optName string) {
+	p.Options[optName] = make([]Vote, 0)
+}
+
+func (p *Poll) Results() map[string]uint32 {
+	result := make(map[string]uint32)
+
+	for k, v := range p.Options {
+		result[k] = uint32(len(v))
+	}
+
+	return result
+}
+
+func (p *Poll) Vote(voterID string, optName string) error {
+	_, exists := p.Options[optName]
+	if !exists {
+		return errors.New("option " + optName + " doest not exists")
+	}
+
+	for _, v := range p.Options {
+		for i := range v {
+			if v[i].VoterID == voterID {
+				return errors.New("voter " + voterID + " already voted in this poll")
+			}
+		}
+	}
+
+	p.Options[optName] = append(p.Options[optName], Vote{uuid.NewString(), voterID})
+
+	return nil
+}
