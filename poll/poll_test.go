3fd75366eb15255ed8914b0150d60eafb88b7671
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-05-11 20:44:44 -0300

    tests: in suite

--- poll/poll_test.go
+++ poll/poll_test.go
@@ -1,6 +1,8 @@
 package poll_test
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/julianolorenzato/choosely/poll"
@@ -11,11 +13,14 @@ type PollTestSuite struct {
 	suite.Suite
 	question string
 	options  []string
+	poll     poll.Poll
 }
 
 func (s *PollTestSuite) SetupTest() {
 	s.question = "Who is the best Valorant player of all time?"
 	s.options = []string{"Aspas", "Less", "Saadhak", "Cauanzin", "Tuyz"}
+	p, _ := poll.NewPoll(s.question, s.options)
+	s.poll = *p
 }
 
 func TestPoll(t *testing.T) {
@@ -32,24 +37,41 @@ func (s *PollTestSuite) TestNewPoll() {
 	s.NotEmpty(p.Options)
 	s.Len(p.Options, len(s.options))
 
-	// -------------------------------------------
+	// --------------------------------------------------------
 
-	s.question = "W"
-	p, err = poll.NewPoll(s.question, s.options)
-
-	s.Nil(p)
-	s.NotNil(err)
-	s.ErrorContains(err, "poll question must have at least 2 characters")
-
-	// -------------------------------------------
-
-	s.options = []string{}
-	p, err = poll.NewPoll(s.question, s.options)
+	p, err = poll.NewPoll("Z", s.options)
 
 	s.Nil(p)
 	s.NotNil(err)
 	s.ErrorContains(err, "poll question must have at least 2 characters")
 
-	// -------------------------------------------
+	// --------------------------------------------------------
 
+	p, err = poll.NewPoll(strings.Repeat("Z", 51), s.options)
+
+	s.Nil(p)
+	s.NotNil(err)
+	s.ErrorContains(err, "poll question must have a maximum of 50 characteres")
+
+	// --------------------------------------------------------
+
+	p, err = poll.NewPoll(s.question, []string{"Derke"})
+
+	s.Nil(p)
+	s.NotNil(err)
+	s.ErrorContains(err, "poll must have at least 2 option")
+
+	// --------------------------------------------------------
+
+	opts := make([]string, 101)
+
+	for i := range s.options {
+		s.options[i] = "option" + strconv.Itoa(i)
+	}
+
+	p, err = poll.NewPoll(s.question, opts)
+
+	s.Nil(p)
+	s.NotNil(err)
+	s.ErrorContains(err, "poll must have a maximum of 100 options")
 }
