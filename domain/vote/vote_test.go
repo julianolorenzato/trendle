3dc242db0612998383d14935315826635ce9e72f
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-06-08 23:55:48 -0300

    test: domain/vote

--- domain/vote/vote_test.go
+++ domain/vote/vote_test.go
@@ -1,2 +1,25 @@
 package vote_test
 
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/julianolorenzato/choosely/domain/vote"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	a := assert.New(t)
+
+	t.Run("It should create a new vote", func(t *testing.T) {
+		voterID := uuid.NewString()
+		optionsChoosed := []string{"Dog", "Avocado", "Purple"}
+
+		v := vote.New(voterID, optionsChoosed)
+
+		a.NotNil(v)
+		a.Equal(v.VoterID, voterID)
+		a.Equal(v.OptionsChoosed, optionsChoosed)
+		a.NotEmpty(v.CreatedAt)
+	})
+}
