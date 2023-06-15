e784fd2b7cd7402a2decc177c9085ae0ccb5f734
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-06-15 19:21:12 -0300

    refac: votes table up migration

--- migrations/000002_votes-table.up.sql
+++ migrations/000002_votes-table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS votes (
-    id                  UUID NOT NULL PRIMARY KEY,
-    voter_id            UUID NOT NULL,
-    choosen_options     VARCHAR(255)[] NOT NULL,
-    created_at          DATE NOT NULL,
-    poll_id             UUID NOT NULL,
-    FOREIGN KEY(poll_id) REFERENCES polls(id)
+    id                          UUID NOT NULL PRIMARY KEY,
+    voter_id                    UUID NOT NULL,
+    poll_id                     UUID NOT NULL,
+    choosen_options             VARCHAR(255)[] NOT NULL,
+    created_at                  DATE NOT NULL,
+    FOREIGN KEY(poll_id)        REFERENCES polls(id),
+    CONSTRAINT uc_poll_voter    UNIQUE (poll_id, voter_id)
 );
