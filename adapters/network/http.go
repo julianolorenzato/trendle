e5d7d8768b0f5a4a82b1c65c4044b1b40f24b68b
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-07-08 22:22:21 -0300

    feat: PollHandler.CreateNewPoll() & PollHandler.VoteInPoll()

--- adapters/network/http.go
+++ adapters/network/http.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/julianolorenzato/choosely/domain/poll"
@@ -20,9 +21,38 @@ func (h *PollHandler) CreateNewPoll(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.service.CreateNewPoll(poll.CreateNewPollDTO{})
+	var dto poll.CreateNewPollDTO
+
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.service.CreateNewPoll(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
 }
 
 func (h *PollHandler) VoteInPoll(w http.ResponseWriter, r *http.Request) {
-	h.service.VoteInPoll(poll.VoteInPollDTO{})
+	if r.Method != "POST" {
+		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var dto poll.VoteInPollDTO
+
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.service.VoteInPoll(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
